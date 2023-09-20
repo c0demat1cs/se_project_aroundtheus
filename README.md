32a53ccc7220bf2689d5b15d96dcd07d2a613fc0
c0demat1cs <oleg.balitskiy@gmail.com>
2023-09-20 10:42:38 -0400

    Submit Project4 Stage4 v1

--- README.md
+++ README.md
@@ -10,12 +10,15 @@ This project involves creating an interactive webpage where users can add and re
 - Flat BEM file structure
 - A custom form
 - Grid
+- JavaScript
 - Responsive design with media queries
   ![mobile view](../images/mobile.view.png)
 - Images
   ![cards ](../images/cards.png)
 - Handling text overflow
   ![text overlow](../images/textoverflow.png)
+- Modal form opens upon clicking the edit button
+- User is able to update their Name and Description fields
 
 ## Repository
 
--- scripts/index.js
+++ scripts/index.js
@@ -25,8 +25,6 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
-
 // Elements
 
 const profileEditButton = document.querySelector("#profile-edit-button");
@@ -39,11 +37,24 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
 //Functions
 
 function closePopup() {
   profileEditModal.classList.remove("modal__opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  cardTitleEl.textContent = cardData.name;
+  return cardElement;
 }
 
 // Event Handlers
@@ -61,10 +72,14 @@ function handleProfileEditSubmit(e) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-
   profileEditModal.classList.add("modal__opened");
 });
 
-modalCloseButton.addEventListener("click", closePopup());
+modalCloseButton.addEventListener("click", closePopup);
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
+});

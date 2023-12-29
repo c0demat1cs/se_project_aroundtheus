407158884722eb039d14c60004db75f117f6aa36
c0demat1cs <oleg.balitskiy@gmail.com>
2023-12-28 23:41:18 -0500

    fixed naming error on github

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import { UserInfo } from "../components/UserInfo.js";
 import Section from "../components/Section.js";
-import PopupWithForm from "../components/popupWithForm.js";
+import PopupWithForm from "../components/PopupWithForm.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import {
   initialCards,
